38e43d6ed7b1591195d28d74ebbd05c7a7276cae
taeminHan <taemin9705@gmail.com>
2020-12-13 20:37:19 +0900

    환승시스템추가

--- API test.py
+++ API test.py
@@ -2,41 +2,55 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+
 class Bus:
 
-    def __init__(self):
+    def __init__(self, start_station, end_station):
         self.key = 'KCCmg%2BFH81e74ptCVObbpItzvOWSM2dX1EDlZfXckrKdJUAS6DtQhI9VoIQCD9gUHRtIQ4Oo%2Br1Ph5YJIqVWlw%3D%3D'
         self.Bus_url = 'http://ws.bus.go.kr/api/rest/pathinfo/getPathInfoByBus'  # 경로
-        self.station_url = 'http://ws.bus.go.kr/api/rest/pathinfo/getLocationInfo'  #정류장 정보 검색
-        self.startX, self.startY, self.endX, self.endX = '', '', '', ''
+        self.station_url = 'http://ws.bus.go.kr/api/rest/pathinfo/getLocationInfo'  # 정류장 정보 검색
+        self.startX, self.startY, self.endX, self.endY = '', '', '', ''
+        self.start_station = start_station
+        self.end_station = end_station
 
     def FindRoute(self):
-
         findUrl = self.Bus_url + '?serviceKey=' + self.key + '&startX=' + self.startX + '&startY=' + self.startY + '&endX=' + self.endX + '&endY=' + self.endY
 
         res = requests.get(findUrl)
         soup = BeautifulSoup(res.content, 'html.parser')
 
-        print(re.findall(r"\d+", str(soup.select('routeNm')))[0])
-        print(re.findall(r"\d+", str(soup.select('routeId')))[0])
+        # 버스번호 (환승필요시 2개 이상의 버스 리스트 출력) 데이터타입 = list
+        routeBusNumber = re.compile(r"\d+\w").findall(str(soup.select_one('itemlist').select('pathList>routeNm')))
+        # 출력확인용
+        print('버스번호', routeBusNumber)
 
-    def FindStation(self, start_station, end_station):
+        # 노선번호 (환승필요시 2개 이상의 버스 리스트 출력) 데이터타입 = list
+        routeRouteId = re.compile(r"\d+\w").findall(str(soup.select_one('itemlist').select('pathList>routeId')))
+        # 출력확인용
+        print('노선번호', routeRouteId)
 
-        stationUrl = self.station_url + '?serviceKey=' + self.key + '&stSrch=' + start_station
-        res = requests.get(stationUrl)
-        soup = BeautifulSoup(res.content, 'html.parser')
+    def FindStation(self):
+        start_stationUrl = self.station_url + '?serviceKey=' + self.key + '&stSrch=' + self.start_station
+        start_res = requests.get(start_stationUrl)
+        start_soup = BeautifulSoup(start_res.content, 'html.parser')
 
-        self.startX = re.findall(r"\d+.\d+", str(soup.select_one('gpsX')))[0]
-        self.startY = re.findall(r"\d+.\d+", str(soup.select_one('gpsY')))[0]
+        self.startX = re.findall(r"\d+.\d+", str(start_soup.select_one('gpsX')))[0]
+        self.startY = re.findall(r"\d+.\d+", str(start_soup.select_one('gpsY')))[0]
+        print("출발 정류장번호", re.findall(r"\d+.\d+", str(start_soup.select_one('poiId')))[0])
 
-        stationUrl = self.station_url + '?serviceKey=' + self.key + '&stSrch=' + end_station
-        res = requests.get(stationUrl)
-        soup = BeautifulSoup(res.content, 'html.parser')
+        end_stationUrl = self.station_url + '?serviceKey=' + self.key + '&stSrch=' + self.end_station
+        end_res = requests.get(end_stationUrl)
+        end_soup = BeautifulSoup(end_res.content, 'html.parser')
 
-        self.endX = re.findall(r"\d+.\d+", str(soup.select_one('gpsX')))[0]
-        self.endY = re.findall(r"\d+.\d+", str(soup.select_one('gpsY')))[0]
+        self.endX = re.findall(r"\d+.\d+", str(end_soup.select_one('gpsX')))[0]
+        self.endY = re.findall(r"\d+.\d+", str(end_soup.select_one('gpsY')))[0]
+        print("도착 정류장번호", re.findall(r"\d+.\d+", str(end_soup.select_one('poiId')))[0])
+        print(self.startX, self.startY, self.endX, self.endY)
+
+    def ArrivalBus(self):
+        pass
 
 
-a = Bus()
-a.FindStation('성산2교', '홍대입구')
+a = Bus('홍대입구역', '여의도공원')
+a.FindStation()
 a.FindRoute()
