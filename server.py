a6775c65a8f3955b08f1d096bc6e665b9dd8b705
taeminHan <taemin9705@gmail.com>
2020-12-14 17:40:25 +0900

    멀티쓰레드 재구현

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import re
 import Api
 
 
-def Send(send_queue):
+def Send(send_queue, group):
     client = pymongo.MongoClient(
         "mongodb+srv://Main:1q2w3e4r@cluster0.dbjal.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority")
     db = client.get_database('Register')
@@ -14,12 +14,18 @@ def Send(send_queue):
 
     while True:
         try:
+            print(len(group))
             recv = send_queue.get()
             # 클라이언트 IP, PORT 정규식 표현으로 처리
             client_ip = re.findall(r"(?<=raddr=\(\')\w+\.\w+\.\w+\.\w+", str(recv[1]))
             client_port = re.findall(r'\w+(?=\)>)', str(recv[1]))
             client_ip = str(client_ip[0])
             client_port = int(client_port[0])
+            ipG = []
+            portG = []
+            for i in group:
+                ipG.append(re.findall(r"(?<=raddr=\(\')\w+\.\w+\.\w+\.\w+", str(i)))
+                portG.append(re.findall(r'\w+(?=\)>)', str(i)))
 
             if recv == 'Group Changed':
                 print('Group Changed')
@@ -29,7 +35,7 @@ def Send(send_queue):
             # 로그인
             if re.findall(r"(login)$", category):
                 # MongoDB 로그인 정보 조회
-                if col.find({'ID': str(info[1]), 'PassWord': str(info[2])}):
+                if list(col.find({'ID': str(info[1]), 'PassWord': str(info[2])})):
                     msg = '1'
                     conn.sendto(msg.encode('utf-8'), (client_ip, client_port))
                 else:
@@ -41,19 +47,19 @@ def Send(send_queue):
                 sign = {'ID': str(info[1]), 'PassWord': str(info[2]), 'Name': str(info[3]), 'CARD': str(info[4]),
                         'Phone': str(info[5])}
                 col.insert_one(sign)
-                if col.find(sign):
-                    msg = '1'
+                if list(col.find(sign)):
+                    msg = 1
                     conn.sendto(msg.encode('utf-8'), (client_ip, client_port))
                 else:
-                    msg = '0'
+                    msg = 0
                     conn.sendto(msg.encode('utf-8'), (client_ip, client_port))
 
             elif re.findall(r"(Register_ID)$", category):  # 아이디 중복 검사
-                if col.find({'ID': str(info[1])}):
-                    msg = '1'
+                if list(col.find({'ID': str(info[1])})):
+                    msg = 1
                     conn.sendto(msg.encode('utf-8'), (client_ip, client_port))
                 else:
-                    msg = '0'
+                    msg = 0
                     conn.sendto(msg.encode('utf-8'), (client_ip, client_port))
             elif re.findall("CARD", category):  # 카드 단말기
                 if info[1] == '1':
@@ -68,11 +74,12 @@ def Send(send_queue):
                 a = Api.Bus()
                 a.FindStation(start, end)
                 msg = a.FindRoute()
-                print(msg)
-                conn.sendto(msg.encode('utf-8'), (client_ip, client_port))
+                for i in group:
+                    i.send(msg.encode('utf-8'))
                 client.close()
         except:
             pass
+
 
 
 def Recv(conn, count, send_queue):
@@ -82,7 +89,7 @@ def Recv(conn, count, send_queue):
         send_queue.put([data, conn, count])
         print(data)
 
-
+    print(group)
 if __name__ == '__main__':
 
     send_queue = Queue()
@@ -98,13 +105,12 @@ if __name__ == '__main__':
         conn, addr = server_sock.accept()
         group.append(conn)
         print('Connected ' + str(addr))
-
         if count > 1:
-            thread1 = threading.Thread(target=Send, args=(send_queue,))
+            thread1 = threading.Thread(target=Send, args=(send_queue,group, ))
             thread1.start()
             pass
         else:
-            thread1 = threading.Thread(target=Send, args=(send_queue,))
+            thread1 = threading.Thread(target=Send, args=(send_queue, group, ))
             thread1.start()
 
         thread2 = threading.Thread(target=Recv, args=(conn, count, send_queue,))
